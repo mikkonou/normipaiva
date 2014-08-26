7fe4795fc1d195f451144d499df321cd0cac7409
Mikko Nousiainen <mikko.n85@gmail.com>
2014-08-26 11:17:13 +0300

    Fixtures generate user accounts.

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,81 +1,99 @@
-if (People.find().count() === 0) {
-  People.insert({
+var users = [
+  {
+    username: 'janne',
     name: 'Janne Henriksson',
     contentmentLevel: 'low',
     workAmount: 'more',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e1)
-  });
-
-  People.insert({
+  },
+  {
+    username: 'mikko',
     name: 'Mikko Taavitsa',
     contentmentLevel: 'high',
     workAmount: 'more',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e2)
-  });
-
-  People.insert({
+  },
+  {
+    username: 'jussi',
     name: 'Jussi Pattitussi',
     contentmentLevel: 'average',
     workAmount: 'same',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e3)
-  });
-
-  People.insert({
+  },
+  {
+    username: 'kikka',
     name: 'Kikka Korea',
     contentmentLevel: 'low',
     workAmount: 'same',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e4)
-  });
-
-  People.insert({
+  },
+  {
+    username: 'risto',
     name: 'Risto Autere',
     contentmentLevel: 'high',
     workAmount: 'less',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e5)
-  });
-
-  People.insert({
-    name: 'Suntio Simo Kuassimo',
+  },
+  {
+    username: 'simo',
+    name: 'Simo Kuassimo',
     contentmentLevel: 'average',
     workAmount: 'less',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e6)
-  });
-
-  People.insert({
+  },
+  {
+    username: 'ari',
     name: 'Ari Zwang',
     contentmentLevel: 'low',
     workAmount: 'less',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e5)
-  });
-
-  People.insert({
+  },
+  {
+    username: 'matti',
     name: 'Matti Näsä',
     contentmentLevel: 'high',
     workAmount: 'same',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e4)
-  });
-
-  People.insert({
+  },
+  {
+    username: 'mauno',
     name: 'Mauno Ahonen',
     contentmentLevel: 'average',
     workAmount: 'more',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e3)
-  });
-
-  People.insert({
+  },
+  {
+    username: 'timo',
     name: 'Timo Silakka',
     contentmentLevel: 'high',
     workAmount: 'more',
     workingOn: 'KobraTK',
     updated: (new Date().getTime() / 1000) - (Math.random() * 10e2)
-  });
+  }
+];
+
+if (Meteor.users.find().count() === 0) {
+  users.forEach( function(user) {
+    var userId = Accounts.createUser({
+      username: user.username,
+      password: 'asdfasdf',
+    });
+    People.insert({
+      id: userId,
+      name: user.name,
+      contentmentLevel: user.contentmentLevel,
+      workAmount: user.workAmount,
+      workingOn: user.workingOn,
+      updated: user.updated
+    })
+  })
 }
