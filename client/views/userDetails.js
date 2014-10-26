d78e3baf0e2a64c46487fa947d6570abe7ce7b45
Mikko Nousiainen <mikko.n85@gmail.com>
2014-10-26 20:05:16 +0200

    Form validation; refactoring.

--- client/views/userDetails.js
+++ client/views/userDetails.js
@@ -1,16 +1,16 @@
 Template.userDetails.events({
-  'submit form': function(e, t) {
+  'submit form': function(e) {
     e.preventDefault();
 
     var _userId           = Meteor.userId()
       , _contentmentLevel = $('.ui.rating').rating('get rating')
-      , _workAmount       = $('.ui.button.workamount.active').val()
-      , _workingOn        = t.find('#name-of-project').value
+      , _workAmount       = $('.ui.button.workamount.active').attr('id')
+      , _workingOn        = $('#name-of-project').val()
       , _timestamp        = new Date().getTime();
 
-    if      (_workAmount == "Less") { _workAmount = 1; }
-    else if (_workAmount == "Same") { _workAmount = 2; }
-    else if (_workAmount == "More") { _workAmount = 3; }
+    if      (_workAmount == 'button-less') { _workAmount = 1; }
+    else if (_workAmount == 'button-same') { _workAmount = 2; }
+    else if (_workAmount == 'button-more') { _workAmount = 3; }
 
     Entries.insert({
       userId: _userId,
@@ -21,15 +21,33 @@ Template.userDetails.events({
     });
 
     Router.go('summary');
+  },
+
+  'click .ui.button.workamount': function(e) {
+    console.log(e);
+    $('.ui.button.workamount').not(e.target).removeClass('active');
+    $(e.target).addClass('active');
   }
 });
 
 Template.userDetails.rendered = function () {
   $('.ui.rating').rating('enable');
   $('.ui.rating').rating('set rating', '5');
-  
-  $('.ui.button.workamount').click( function () {
-    $('.ui.button.workamount').removeClass('active');
-    $(this).addClass('active');
+
+  //Semantic UI client side validation
+  $('#new-entry-form')
+  .form({
+    workingOn: {
+      identifier  : 'name-of-project',
+      rules: [
+        {
+          type   : 'empty',
+          prompt : 'Please let us know what you are working on'
+        }
+      ]
+    }
+  },
+  {
+    inline: 'true'
   });
 }
