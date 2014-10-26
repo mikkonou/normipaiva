ec6c0f431d1d65d06ef703a55bc741128c4e2bcd
Mikko Nousiainen <mikko.n85@gmail.com>
2014-10-26 13:53:47 +0200

    Fixed summary view behaviour when user has no entries.

--- client/views/summary.js
+++ client/views/summary.js
@@ -4,9 +4,11 @@ Template.summary.helpers({
     var people = People.find();
     people.forEach( function(person) {
       var entry = (Entries.findOne({userId: person.userId}, {sort: {timestamp: -1}}));
-      entry.firstName = person.first_name;
-      entry.lastName  = person.last_name;
-      unsorted.push(entry);
+      if (entry) {
+        entry.firstName = person.first_name;
+        entry.lastName  = person.last_name;
+        unsorted.push(entry);
+      }
     });
     var result = _(unsorted).sortBy(function(entry) { return -entry.timestamp })
     return result;
