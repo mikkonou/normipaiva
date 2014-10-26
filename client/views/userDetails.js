f65b21cf80c7b75a81831b8dbdb69e193cf87e0a
Mikko Nousiainen <mikko.n85@gmail.com>
2014-10-26 13:31:34 +0200

    Fixed workload buttons.

--- client/views/userDetails.js
+++ client/views/userDetails.js
@@ -4,9 +4,13 @@ Template.userDetails.events({
 
     var _userId           = Meteor.userId()
       , _contentmentLevel = $('.ui.rating').rating('get rating')
-      , _workAmount       = $('.ui.button.active').val()
+      , _workAmount       = $('.ui.button.workamount.active').val()
       , _workingOn        = t.find('#name-of-project').value
       , _timestamp        = new Date().getTime();
+
+    if      (_workAmount == "Less") { _workAmount = 1; }
+    else if (_workAmount == "Same") { _workAmount = 2; }
+    else if (_workAmount == "More") { _workAmount = 3; }
 
     Entries.insert({
       userId: _userId,
@@ -24,8 +28,8 @@ Template.userDetails.rendered = function () {
   $('.ui.rating').rating('enable');
   $('.ui.rating').rating('set rating', '5');
   
-  $('.ui.button').click( function () {
-    $('.ui.button').removeClass('active');
+  $('.ui.button.workamount').click( function () {
+    $('.ui.button.workamount').removeClass('active');
     $(this).addClass('active');
   });
 }
